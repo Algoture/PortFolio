f23d1b746ad7726f07ee28572a37a46a3be548de
Umesh Nagare <umeshnagare27@gmail.com>
2024-07-20 18:54:14 +0530

    Added ScrollTrigger

---
+++ script.js
@@ -0,0 +1,32 @@
+// const scroller = new LocomotiveScroll({
+//   el: document.querySelector("[data-scroll-container]"),
+//   smooth: true,
+// });
+
+// gsap.register(scrollTrigger);
+gsap.to("#mainTitle", {
+  scrollTrigger: {
+    trigger: "#mainTitle",
+    scroller: "body",
+    markers: false,
+    scrub: 2,
+    start: "top 405",
+    end: "bottom 20%",
+    ease: "power1.inOut",
+  },
+  x: 100,
+  duration: 4,
+});
+// gsap.from("#whoIam", {
+//   scrollTrigger: {
+//     trigger: "#whoIam",
+//     scroller: "body",
+//     markers: false,
+//     scrub: 2,
+//     start: "top 80%%",
+//     end: "bottom 20%",
+//     ease: "power1.inOut",
+//   },
+//   x: 100,
+//   duration: 4,
+// });
