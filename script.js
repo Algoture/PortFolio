ee2809e742355db3b2c49082755cd3c4e586a8ee
Umesh Nagare <umeshnagare27@gmail.com>
2024-07-20 18:57:29 +0530

    Removed animation

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 // });
 
 // gsap.register(scrollTrigger);
-gsap.to("#mainTitle", {
-  scrollTrigger: {
-    trigger: "#mainTitle",
-    scroller: "body",
-    markers: false,
-    scrub: 2,
-    start: "top 405",
-    end: "bottom 20%",
-    ease: "power1.inOut",
-  },
-  x: 100,
-  duration: 4,
-});
+// gsap.to("#mainTitle", {
+//   scrollTrigger: {
+//     trigger: "#mainTitle",
+//     scroller: "body",
+//     markers: false,
+//     scrub: 2,
+//     start: "top 405",
+//     end: "bottom 20%",
+//     ease: "power1.inOut",
+//   },
+//   x: 100,
+//   duration: 4,
+// });
 // gsap.from("#whoIam", {
 //   scrollTrigger: {
 //     trigger: "#whoIam",
