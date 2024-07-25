16de5afcccdd058224550d87d07f28d7923c39e8
Umesh Nagare <umeshnagare27@gmail.com>
2024-07-25 23:40:20 +0530

    Scroll Trigger Configured with Locomotive

--- script.js
+++ script.js
@@ -1,32 +1,53 @@
-const scroller = new LocomotiveScroll({
-  el: document.querySelector("[data-scroll-container]"),
-  smooth: true,
+document.addEventListener("DOMContentLoaded", function () {
+  const scroller = new LocomotiveScroll({
+    el: document.querySelector("[data-scroll-container]"),
+    smooth: true,
+  });
+  gsap.registerPlugin(ScrollTrigger);
+  scroller.on("scroll", ScrollTrigger.update);
+  ScrollTrigger.scrollerProxy("[data-scroll-container]", {
+    scrollTop(value) {
+      return arguments.length
+        ? scroller.scrollTo(value, 0, 0)
+        : scroller.scroll.instance.scroll.y;
+    },
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    pinType: document.querySelector("[data-scroll-container]").style.transform
+      ? "transform"
+      : "fixed",
+  });
+  ScrollTrigger.addEventListener("refresh", () => scroller.update());
+  ScrollTrigger.refresh();
+  gsap.to("#whoIam", {
+    scrollTrigger: {
+      trigger: "#whoIam",
+      scroller: "[data-scroll-container]",
+      markers: false,
+      scrub: 1,
+      start: "top 30%",
+      end: "bottom 20%",
+      ease: "power1.inOut",
+    },
+    x: 100,
+  });
+  gsap.from("#mainTitle", {
+    scrollTrigger: {
+      trigger: "#mainTitle",
+      scroller: "[data-scroll-container]",
+      markers: false,
+      scrub: 1,
+      start: "top 30%",
+      end: "bottom 20%",
+      ease: "power1.inOut",
+    },
+    x: 100,
+    duration: 4,
+  });
 });
-
-// gsap.register(scrollTrigger);
-// gsap.to("#mainTitle", {
-//   scrollTrigger: {
-//     trigger: "#mainTitle",
-//     scroller: "body",
-//     markers: false,
-//     scrub: 2,
-//     start: "top 405",
-//     end: "bottom 20%",
-//     ease: "power1.inOut",
-//   },
-//   x: 100,
-//   duration: 4,
-// });
-// gsap.from("#whoIam", {
-//   scrollTrigger: {
-//     trigger: "#whoIam",
-//     scroller: "body",
-//     markers: false,
-//     scrub: 2,
-//     start: "top 80%%",
-//     end: "bottom 20%",
-//     ease: "power1.inOut",
-//   },
-//   x: 100,
-//   duration: 4,
-// });
