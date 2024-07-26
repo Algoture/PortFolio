4f16b189b5786be992d3070b61c8b39a98142834
Umesh Nagare <umeshnagare27@gmail.com>
2024-07-26 16:07:33 +0530

    Hacket Text Effect added

--- script.js
+++ script.js
@@ -35,19 +35,66 @@ document.addEventListener("DOMContentLoaded", function () {
   //     end: "bottom 20%",
   //     ease: "power1.inOut",
   //   },
-  //   x: 50,
+  //   x: 100,
   //   duration: 4,
   // });
-  gsap.from("#arrowsvg", {
-    scrollTrigger: {
-      trigger: "#arrowsvg",
-      scroller: "[data-scroll-container]",
-      markers: false,
-      scrub: 1,
-      start: "top 90%",
-      end: "bottom 20%",
-      ease: "power1.inOut",
-    },
-    rotate:90,
-  });
+  // gsap.from("#arrowsvg", {
+  //   scrollTrigger: {
+  //     trigger: "#arrowsvg",
+  //     scroller: "[data-scroll-container]",
+  //     markers: false,
+  //     scrub: 1,
+  //     start: "top 90%",
+  //     end: "bottom 20%",
+  //     ease: "power1.inOut",
+  //   },
+  //   rotate: 90,
+  // });
+});
+
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let interval = null;
+let sound = new Audio("Sound2.wav");
+let soundEnabled = false;
+
+document.querySelector("#name").onmouseover = (event) => {
+  let iteration = 0;
+  clearInterval(interval);
+  interval = setInterval(() => {
+    event.target.innerText = event.target.innerText
+      .split("")
+      .map((letter, index) => {
+        if (index < iteration) {
+          return event.target.dataset.value[index];
+        }
+        return letters[Math.floor(Math.random() * 26)];
+      })
+      .join("");
+    if (iteration >= event.target.dataset.value.length) {
+      clearInterval(interval);
+    }
+    iteration += 1 / 3;
+  }, 40);
+  if (soundEnabled) {
+    sound.play().catch((error) => {
+      console.error("Audio play failed:", error);
+    });
+  }
+};
+
+const nameElement = document.querySelector("#name");
+nameElement.dataset.value = nameElement.innerText;
+
+// Sound :
+const soundButton = document.querySelector("#soundButton");
+const soundsvg = document.querySelector("#soundSVG").querySelector("use");
+soundButton.addEventListener("click", () => {
+  soundEnabled = !soundEnabled;
+  const currentHref = soundsvg.getAttribute("xlink:href");
+  soundsvg.setAttribute(
+    "xlink:href",
+    currentHref === "Icons.svg#soundOff"
+      ? "Icons.svg#soundOn"
+      : "Icons.svg#soundOff"
+  );
 });
