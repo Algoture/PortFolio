731dd545bd762aa00e4dc93d8d8583a15a0e2e27
Umesh Nagare <umeshnagare27@gmail.com>
2024-10-29 23:30:09 +0530

    again fix

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/terminal" element={<Terminal />} />
+          <Route path="https://umeshnagare.vercel.app/terminal" element={<Terminal />} />
           <Route path="*" element={<MainBody />} />
         </Routes>
       </Router>
