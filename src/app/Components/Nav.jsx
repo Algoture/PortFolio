6b7034a12b267b3b01d2a3dcf41bd44a8740379e
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-09 19:09:05 +0530

    discernible name added

--- src/app/Components/Nav.jsx
+++ src/app/Components/Nav.jsx
@@ -12,34 +12,41 @@ import {
 const links = [
   {
     href: "https://github.com/Algoture",
+    name: "GitHub",
     target: "_blank",
     Icon: GitHubIcon,
   },
   {
     href: "/terminal",
+    name: "Terminal",
     Icon: TerminalIcon,
   },
   {
     href: "https://x.com/umeshn22",
+    name: "Twitter",
     target: "_blank",
     Icon: TwitterIcon,
   },
   {
     href: "/",
+    name: "Home",
     Icon: HomeIcon,
   },
   {
     href: "https://www.instagram.com/2.0_27/",
+    name: "Instagram",
     target: "_blank",
     Icon: InstaIcon,
   },
   {
     href: "https://www.linkedin.com/in/umesh27/",
+    name: "LinkedIn",
     target: "_blank",
     Icon: LinkedInIcon,
   },
   {
     href: "https://discord.com/users/790565393659330591",
+    name: "Discord",
     target: "_blank",
     Icon: DiscordIcon,
   },
@@ -49,14 +56,14 @@ const Nav = () => {
   return (
     <section id="dock">
       <nav>
-        {links.map(({ href, target, Icon }, index) => (
+        {links.map(({ href, target, Icon, name }, index) => (
           <Link
             key={index}
-            className=""
+            aria-label={name}
             href={href}
             target={target || undefined}
           >
-            <Icon className="size-7" />
+            <Icon className="md:size-7 size-9" />
           </Link>
         ))}
       </nav>
