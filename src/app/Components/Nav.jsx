b74a8d979f08b29c6e82a2922bfc4d91f07ac9f0
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-22 14:40:08 +0530

    add : discernible names to links and btns

--- src/app/Components/Nav.jsx
+++ src/app/Components/Nav.jsx
@@ -15,14 +15,15 @@ const Nav = () => {
   return (
     <div className="fixed bottom-2 right-1/2 transform translate-x-1/2 z-40">
       <nav className="flex px-3 py-2 gap-3 rounded-full bg-accent dark:bg-gray-100 shadow-sh">
-        {links.map(({ href, target, Icon }, index) => (
-          <Link key={index} href={href} target={target || "_self"}>
+        {links.map(({ href, target, Icon, name }, index) => (
+          <Link key={index} href={href} target={target} aria-label={name}>
             {Icon}
           </Link>
         ))}
         <div className="border-l-2 dark:border-accent flex items-center pl-2">
           <button
             onClick={() => darkModeHandler()}
+            aria-label="Dark Mode Toggle"
             className={clsx(
               "cen transition-all rounded-full focus:outline-none",
               dark ? "rotate-0" : "rotate-90"
