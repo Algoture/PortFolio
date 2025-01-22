c2c5bcb1149bba4fffa2e027314a1f1f0ee15ea9
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-22 14:30:48 +0530

    warning fix

--- src/app/Utils/Data.js
+++ src/app/Utils/Data.js
@@ -131,5 +131,5 @@ export const transition = { duration: 1, ease: [0.25, 0.1, 0.25, 1] };
 
 export const variants = {
   hidden: { filter: "blur(10px)", transform: "translateY(20%)", opacity: 0 },
-  visible: { filter: "blur(0)", transform: "translateY(0)", opacity: 1 },
+  visible: { filter: "blur(0px)", transform: "translateY(0%)", opacity: 1 },
 };
