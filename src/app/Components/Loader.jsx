7571606b92d474714a1264550bdf3a16884ce467
Umesh Nagare <umeshnagare27@gmail.com>
2025-01-31 12:14:04 +0530

    update : Loader

---
+++ src/app/Components/Loader.jsx
@@ -0,0 +1,18 @@
+import "./loader.css";
+const Loader = () => (
+  <div className="spinner center">
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+    <div className="spinner-blade"></div>
+  </div>
+);
+export default Loader;
--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -2,6 +2,7 @@
 import React, { useEffect, useState } from "react";
 import CountUp from "../Components/CountUp";
 import { BlockchainIcon, FireIcon } from "../Components/Icons";
+import Loader from "../Components/Loader";
 
 const Page = () => {
   const [data, setData] = useState({
@@ -29,15 +30,7 @@ const Page = () => {
     }
   }
   if (loading) {
-    return (
-      <div className="flex justify-center items-center h-screen">
-        <div className="relative inline-flex">
-          <div className="w-8 h-8 bg-accent rounded-full"></div>
-          <div className="w-8 h-8 bg-accent rounded-full absolute top-0 left-0 animate-ping"></div>
-          <div className="w-8 h-8 bg-accent rounded-full absolute top-0 left-0 animate-pulse"></div>
-        </div>
-      </div>
-    );
+    return <Loader />;
   }
 
   return (
